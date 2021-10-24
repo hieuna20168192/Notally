8160b9b81145664f58ce7b1fbfd2931e42e03d0e
hieuna7 <hieuna7@ghtk.co>
2021-10-24 10:29:17 +0700

    init base MVVM

---
+++ app/src/main/java/com/example/notally/base/BaseActivity.kt
@@ -0,0 +1,42 @@
+package com.example.notally.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.annotation.StyleRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+abstract class BaseActivity<VM : BaseViewModel, B : ViewDataBinding>:
+    AppCompatActivity(), IBaseScreen {
+
+    @get: LayoutRes
+    protected abstract val layoutId: Int
+
+    @get: StyleRes
+    protected var themeId: Int? = null
+
+    protected lateinit var binding: B
+
+    protected abstract val viewModel: VM
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        themeId?.let {
+            setTheme(it)
+        }
+
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        binding.lifecycleOwner = this
+
+        initComponents()
+        initListeners()
+    }
+
+    abstract fun initComponents()
+
+    abstract fun initListeners()
+}
---
+++ app/src/main/java/com/example/notally/base/BaseFragment.kt
@@ -0,0 +1,37 @@
+package com.example.notally.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+abstract class BaseFragment<VM : BaseViewModel, B : ViewDataBinding> :
+    Fragment(), IBaseScreen {
+
+    @get: LayoutRes
+    protected abstract val layoutId: Int
+
+    protected lateinit var binding: B
+    protected abstract val viewModel: VM
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        binding.lifecycleOwner = this
+        initComponents()
+        initListeners()
+        return binding.root
+    }
+
+    protected abstract fun initComponents()
+    protected abstract fun initListeners()
+}
